cfee6adf7c87b0068ea517e8dfe25a1eb7d3ba76
이름 <>
2021-01-08 16:02:37 +0800

    message

---
+++ commands/nowplaying.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../util/error")
+
+module.exports = {
+  info: {
+    name: "재생목록",
+    description: "To show the music which is currently playing in this server",
+    usage: "",
+    aliases: ["np"],
+  },
+
+  run: async function (client, message, args) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+    let song = serverQueue.songs[0]
+    let thing = new MessageEmbed()
+      .setAuthor("지금 재생중", "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+      .setThumbnail(song.img)
+      .setColor("BLUE")
+      .addField("Name", song.title, true)
+      .addField("Duration", song.duration, true)
+      .addField("Requested by", song.req.tag, true)
+      .setFooter(`Views: ${song.views} | ${song.ago}`)
+    return message.channel.send(thing)
+  },
+};
